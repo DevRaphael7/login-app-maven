6f0c68ecacdfea8fb8f3ec28fe1c2d2347f8f644
RAPHAEL RAMALHO DOS SANTOS <raphael2000840@creasp.org.br>
2022-07-14 11:32:29 -0300

    Request token in api

--- src/main/webapp/pages/loginPage/scripts/login-page.script.js
+++ src/main/webapp/pages/loginPage/scripts/login-page.script.js
@@ -39,9 +39,61 @@ var btnLogin = document.getElementById("btn-login");
 var usuarioHtmlInput = document.querySelector("[usuario]");
 var senhaHtmlInput = document.querySelector("[senha]");
 var spinnerContainer = document.querySelector('[spinnerContainer]');
+var TokenService = /** @class */ (function () {
+    function TokenService() {
+        this.endPoint = false ? 'https://login-register-app-node.herokuapp.com/api/login' : 'http://localhost:9000/api/login';
+        this.token = "";
+    }
+    TokenService.prototype.setJsonMimeTypeInOptionsRequest = function () {
+        this.optionsRequest = {
+            method: 'POST',
+            body: JSON.stringify({
+                user: "@raphael",
+                password: "123"
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    };
+    TokenService.prototype.requestTokenApi = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var response, data, token, ex_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 3, , 4]);
+                        this.setJsonMimeTypeInOptionsRequest();
+                        return [4 /*yield*/, fetch(this.endPoint, this.optionsRequest)];
+                    case 1:
+                        response = _a.sent();
+                        return [4 /*yield*/, response.json()];
+                    case 2:
+                        data = _a.sent();
+                        if (response.ok) {
+                            token = data.token;
+                            window.localStorage.setItem('token', token);
+                            return [2 /*return*/, true];
+                        }
+                        else {
+                            return [2 /*return*/, false];
+                        }
+                        return [3 /*break*/, 4];
+                    case 3:
+                        ex_1 = _a.sent();
+                        console.log(ex_1);
+                        return [2 /*return*/, false];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    return TokenService;
+}());
 var LoginPage = /** @class */ (function () {
     function LoginPage() {
         var _this = this;
+        this.urlApi = false ? 'https://login-register-app-node.herokuapp.com/api/loginUser' : 'http://localhost:9000/api/loginUser';
         this.getNome = function () { return _this.nome; };
         this.getSenha = function () { return _this.senha; };
         this.formHtml = document.querySelector("[formLogin]");
@@ -50,6 +102,7 @@ var LoginPage = /** @class */ (function () {
         this.optionsRequest = {};
         this.formHtml.addEventListener('submit', function (e) { return e.preventDefault(); });
         this.errorMessage = document.querySelector('[errorMessage]');
+        this.token = new TokenService();
     }
     LoginPage.prototype.setNome = function (nome) {
         if (!nome)
@@ -72,7 +125,7 @@ var LoginPage = /** @class */ (function () {
     };
     LoginPage.prototype.requestLoginApi = function () {
         return __awaiter(this, void 0, void 0, function () {
-            var response, data, ex_1;
+            var checkGetToken, response, data, ex_2;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
@@ -81,29 +134,36 @@ var LoginPage = /** @class */ (function () {
                             name: this.nome,
                             password: this.senha
                         });
-                        _a.label = 1;
+                        return [4 /*yield*/, this.token.requestTokenApi()];
                     case 1:
-                        _a.trys.push([1, 4, , 5]);
-                        return [4 /*yield*/, fetch('https://login-register-app-node.herokuapp.com/api/loginUser', this.optionsRequest)];
+                        checkGetToken = _a.sent();
+                        _a.label = 2;
                     case 2:
+                        _a.trys.push([2, 5, , 6]);
+                        if (!checkGetToken)
+                            throw Error('Error na  requisição do token');
+                        console.log(window.localStorage.getItem('token'));
+                        return [4 /*yield*/, fetch(this.urlApi, this.optionsRequest)];
+                    case 3:
                         response = _a.sent();
                         return [4 /*yield*/, response.json()];
-                    case 3:
+                    case 4:
                         data = _a.sent();
                         if (!response.ok) {
                             this.setErrorMessage(data.message);
+                            this.exibirLoading(false);
                             return [2 /*return*/];
                         }
                         console.log(data);
                         this.exibirLoading(false);
-                        return [3 /*break*/, 5];
-                    case 4:
-                        ex_1 = _a.sent();
-                        console.log(ex_1);
-                        this.setErrorMessage('Ops ocorreu um erro na requisição');
+                        return [3 /*break*/, 6];
+                    case 5:
+                        ex_2 = _a.sent();
+                        console.log(ex_2);
+                        this.setErrorMessage('Ops ocorreu um erro ' + ex_2);
                         this.exibirLoading(false);
-                        return [3 /*break*/, 5];
-                    case 5: return [2 /*return*/];
+                        return [3 /*break*/, 6];
+                    case 6: return [2 /*return*/];
                 }
             });
         });
--- src/main/webapp/pages/loginPage/scripts/login-page.script.ts
+++ src/main/webapp/pages/loginPage/scripts/login-page.script.ts
@@ -11,10 +11,58 @@ interface User {
     password: string;
 }
 
+interface TokenResponse {
+    currentDate: string;
+    dateTimeExpire: string;
+    token: string;
+}
+
 interface ErrorResponse {
     code: number;
     message: string;
     operation: boolean;
+}
+
+class TokenService {
+
+    private token: string;
+    private endPoint = false ? 'https://login-register-app-node.herokuapp.com/api/login' : 'http://localhost:9000/api/login';
+    private optionsRequest: any;
+
+    constructor() {
+        this.token = "";
+    }
+
+    private setJsonMimeTypeInOptionsRequest() {
+        this.optionsRequest = {
+            method: 'POST',
+            body: JSON.stringify({
+                user: "@raphael",
+                password: "123"
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    }
+
+    public async requestTokenApi() {
+        try{
+            this.setJsonMimeTypeInOptionsRequest();
+            const response = await fetch(this.endPoint, this.optionsRequest);
+            const data = await response.json();
+            if(response.ok){
+                const token = (data as TokenResponse).token;
+                window.localStorage.setItem('token', token);
+                return true;
+            } else {
+                return false;
+            }
+        } catch(ex) {
+            console.log(ex)
+            return false;
+        }
+    }
 }
 
 class LoginPage{
@@ -24,6 +72,9 @@ class LoginPage{
     private formHtml: HTMLElement;
     private optionsRequest: any;
     private errorMessage: HTMLElement;
+    private token: TokenService; 
+
+    private urlApi = false ? 'https://login-register-app-node.herokuapp.com/api/loginUser' : 'http://localhost:9000/api/loginUser';
 
     constructor() {
         this.formHtml = document.querySelector("[formLogin]") as HTMLElement;
@@ -32,6 +83,7 @@ class LoginPage{
         this.optionsRequest = {};
         this.formHtml.addEventListener('submit', e => e.preventDefault());
         this.errorMessage = document.querySelector('[errorMessage]') as HTMLElement;
+        this.token = new TokenService();
     }
 
     public getNome = () => this.nome;
@@ -64,18 +116,26 @@ class LoginPage{
             password: this.senha
         })
 
+        const checkGetToken = await this.token.requestTokenApi();
+
         try{
-            const response = await fetch('https://login-register-app-node.herokuapp.com/api/loginUser', this.optionsRequest)
+
+            if(!checkGetToken) throw Error('Error na  requisição do token');
+
+            console.log(window.localStorage.getItem('token'))
+            
+            const response = await fetch(this.urlApi, this.optionsRequest)
             const data = await response.json();
             if(!response.ok){
                 this.setErrorMessage((data as ErrorResponse).message)
+                this.exibirLoading(false);
                 return;
             }
-            console.log(data)
+            console.log(data);
             this.exibirLoading(false);
         } catch(ex) {
             console.log(ex)
-            this.setErrorMessage('Ops ocorreu um erro na requisição');
+            this.setErrorMessage('Ops ocorreu um erro ' + ex);
             this.exibirLoading(false);
         }
     }
