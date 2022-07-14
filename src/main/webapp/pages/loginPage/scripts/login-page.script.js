f79d3366947949020ead943afc6f303c1ef74036
RAPHAEL RAMALHO DOS SANTOS <raphael2000840@creasp.org.br>
2022-07-14 09:01:47 -0300

    Make fecth and error message

--- src/main/webapp/pages/loginPage/scripts/login-page.script.js
+++ src/main/webapp/pages/loginPage/scripts/login-page.script.js
@@ -1,3 +1,40 @@
+// import { fetch } from 'cross-fetch';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var btnLogin = document.getElementById("btn-login");
 var usuarioHtmlInput = document.querySelector("[usuario]");
 var senhaHtmlInput = document.querySelector("[senha]");
@@ -9,12 +46,13 @@ var LoginPage = /** @class */ (function () {
         this.formHtml = document.querySelector("[formLogin]");
         this.nome = "";
         this.senha = "";
+        this.optionsRequest = {};
         this.formHtml.addEventListener('submit', function (e) { return e.preventDefault(); });
+        this.errorMessage = document.querySelector('[errorMessage]');
     }
     LoginPage.prototype.setNome = function (nome) {
         if (!nome)
             return;
-        console.log(nome);
         this.nome += nome.replace("Key", "");
     };
     LoginPage.prototype.setSenha = function (senha) {
@@ -22,15 +60,59 @@ var LoginPage = /** @class */ (function () {
             return;
         this.senha += senha.replace("Key", "");
     };
-    LoginPage.prototype.loginEvent = function () {
+    LoginPage.prototype.setJsonMimeTypeInOptionsRequest = function (user) {
+        this.optionsRequest = {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    };
+    LoginPage.prototype.requestLoginApi = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var response, data, ex_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        this.setJsonMimeTypeInOptionsRequest({
+                            name: this.nome,
+                            password: this.senha
+                        });
+                        _a.label = 1;
+                    case 1:
+                        _a.trys.push([1, 4, , 5]);
+                        return [4 /*yield*/, fetch('https://login-register-app-node.herokuapp.com/api/loginUser', this.optionsRequest)];
+                    case 2:
+                        response = _a.sent();
+                        return [4 /*yield*/, response.json()];
+                    case 3:
+                        data = _a.sent();
+                        console.log(data);
+                        if (!response.ok) {
+                            this.setErrorMessage(data.message);
+                            return [2 /*return*/];
+                        }
+                        console.log(data);
+                        return [3 /*break*/, 5];
+                    case 4:
+                        ex_1 = _a.sent();
+                        console.log(ex_1);
+                        this.setErrorMessage('Ops ocorreu um erro na requisição');
+                        return [3 /*break*/, 5];
+                    case 5: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    LoginPage.prototype.setErrorMessage = function (message) {
+        this.errorMessage.textContent = message;
     };
     return LoginPage;
 }());
 var loginPage = new LoginPage();
 usuarioHtmlInput.addEventListener('keydown', function (e) { return loginPage.setNome(e.code); });
 senhaHtmlInput.addEventListener('keydown', function (e) { return loginPage.setSenha(e.code); });
-btnLogin.addEventListener('click', function (e) {
-    console.log(loginPage.getNome());
-    console.log(loginPage.getSenha());
-    e.preventDefault();
+btnLogin.addEventListener('click', function () {
+    loginPage.requestLoginApi();
 });
--- src/main/webapp/pages/loginPage/scripts/login-page.script.ts
+++ src/main/webapp/pages/loginPage/scripts/login-page.script.ts
@@ -1,18 +1,35 @@
+import { fetch } from 'cross-fetch';
+
 const btnLogin = document.getElementById("btn-login") as HTMLElement;
 const usuarioHtmlInput = document.querySelector("[usuario]") as HTMLElement;
 const senhaHtmlInput = document.querySelector("[senha]") as HTMLElement;
+
+interface User {
+    name: string;
+    password: string;
+}
+
+interface ErrorResponse {
+    code: number;
+    message: string;
+    operation: boolean;
+}
 
 class LoginPage{
 
     private nome: string;
     private senha: string;
     private formHtml: HTMLElement;
+    private optionsRequest: any;
+    private errorMessage: HTMLElement;
 
     constructor() {
         this.formHtml = document.querySelector("[formLogin]") as HTMLElement;
         this.nome = ""
         this.senha = "";
+        this.optionsRequest = {};
         this.formHtml.addEventListener('submit', e => e.preventDefault());
+        this.errorMessage = document.querySelector('[errorMessage]') as HTMLElement;
     }
 
     public getNome = () => this.nome;
@@ -28,8 +45,39 @@ class LoginPage{
         this.senha += senha.replace("Key", "");
     }
 
-    public loginEvent() {
+    private setJsonMimeTypeInOptionsRequest(user: User) {
+        this.optionsRequest = {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    }
 
+    public async requestLoginApi(): Promise<void> {
+        this.setJsonMimeTypeInOptionsRequest({
+            name: this.nome,
+            password: this.senha
+        })
+
+        try{
+            const response = await fetch('https://login-register-app-node.herokuapp.com/api/loginUser', this.optionsRequest)
+            const data = await response.json();
+            if(!response.ok){
+                this.setErrorMessage((data as ErrorResponse).message)
+                return;
+            }
+
+            console.log(data)
+        } catch(ex) {
+            console.log(ex)
+            this.setErrorMessage('Ops ocorreu um erro na requisição')
+        }
+    }
+
+    private setErrorMessage(message: string) {
+        this.errorMessage.textContent = message;
     }
 }
 
@@ -38,8 +86,6 @@ const loginPage = new LoginPage();
 usuarioHtmlInput.addEventListener('keydown', e => loginPage.setNome(e.code));
 senhaHtmlInput.addEventListener('keydown', e => loginPage.setSenha(e.code));
 
-btnLogin.addEventListener('click', (e) => {
-    console.log(loginPage.getNome());
-    console.log(loginPage.getSenha());
-    e.preventDefault();
+btnLogin.addEventListener('click', () => {
+    loginPage.requestLoginApi();
 })
